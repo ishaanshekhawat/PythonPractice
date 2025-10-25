6d1ded15bba00479e273643d2e69a24efe0376d0
Ishaan Singh Shekhawat <ishaan.singh.shekhawat@gmail.com>
2025-10-25 23:46:35 +0530

    Update Re.split().py

--- Re.split().py
+++ Re.split().py
@@ -1,4 +1,11 @@
-regex_pattern = r"[,.]"	# Do not delete 'r'.
+# Define a regular expression (regex) pattern to match commas (,) and periods (.)
+regex_pattern = r"[,.]"  # Do not delete 'r'
 
+# Import the regular expressions module
 import re
+
+# Take input from the user as a string
+# Use re.split() to split the string wherever a comma or period appears
+# Then, join the resulting list elements with newline characters ('\n')
+# This effectively prints each segment of the input string on a new line.
 print("\n".join(re.split(regex_pattern, input())))
