d02e81c92ae09d22a93cf80d978d986a07324895
Ishaan Singh Shekhawat <ishaan.singh.shekhawat@gmail.com>
2025-10-27 19:18:18 +0530

    Update name.py

--- name.py
+++ name.py
@@ -1,7 +1,13 @@
+# Function to print a personalized greeting message using the provided first and last name
 def print_full_name(first, last):
-    print(f'Hello {first_name} {last_name}! You just delved into python.')
+    print(f'Hello {first} {last}! You just delved into python.')
 
+# Main code block that executes when the script is run directly
 if __name__ == '__main__':
+    # Read the user's first name as input
     first_name = input()
+    # Read the user's last name as input
     last_name = input()
+    
+    # Call the function with the provided inputs to display the greeting
     print_full_name(first_name, last_name)
