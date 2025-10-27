5891e1581332101033a8a86cac8cc0e04ec39978
Ishaan Singh Shekhawat <ishaan.singh.shekhawat@gmail.com>
2025-10-27 19:07:01 +0530

    Update Doormat.py

--- Doormat.py
+++ Doormat.py
@@ -1,12 +1,20 @@
+# Read two integers from user input: n (number of rows) and width (total width of the pattern)
 n, width = map(int, input().split())
 
+# Define the pattern to be used for the top and bottom parts of the design
 p = '.|.'
+# Define the message to be printed in the center of the pattern
 sign = 'WELCOME'
 
+# Loop to print the top part of the pattern (above the "WELCOME" sign)
 for i in range(1, n, 2):
-    print((p*i).center(width, '-'))
+    # Print the pattern string 'p' repeated i times, centered in the given width, with '-' as padding
+    print((p * i).center(width, '-'))
 
+# Print the "WELCOME" message centered in the given width, with '-' as padding
 print(sign.center(width, '-'))
 
-for i in range(n-2, 0, -2):
-    print((p*i).center(width, '-'))
+# Loop to print the bottom part of the pattern (below the "WELCOME" sign)
+for i in range(n - 2, 0, -2):
+    # Print the pattern string 'p' repeated i times, centered in the given width, with '-' as padding
+    print((p * i).center(width, '-'))
