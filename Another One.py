f932903249d84a03a9d35f358eeda81ea7ff752b
Ishaan Singh Shekhawat <ishaan.singh.shekhawat@gmail.com>
2025-11-02 17:04:05 +0530

    Update Another One.py

--- Another One.py
+++ Another One.py
@@ -13,13 +13,28 @@
 # Output: [7, 0]
 
 def another_one(digits):
+    # Initialize an empty list to store digits as strings
     l = []
+    
+    # Convert each integer digit to a string and append it to the list
     for i in digits:
         l.append(str(i))
+    
+    # Join all string digits into one full number string, then convert it to an integer
     num = int(''.join(l))
-    num = num+1
+    
+    # Add one to the integer
+    num = num + 1
+    
+    # Convert the incremented number back into a list of string digits
     plusone = list(str(num))
+    
+    # Initialize another list to store the final digits as integers
     l1 = []
+    
+    # Convert each string digit back into an integer
     for i in plusone:
         l1.append(int(i))
+    
+    # Return the list of digits representing the incremented number
     return l1
