587b40169aee63f36023ea43d9861ea84945a410
Ishaan Singh Shekhawat <ishaan.singh.shekhawat@gmail.com>
2025-11-06 23:56:32 +0530

    Add files via upload

---
+++ Average Subarray.py
@@ -0,0 +1,37 @@
+# You are given an integer array nums consisting of n elements, and an integer k.
+
+# Your task is to find a contiguous subarray of nums whose length is exactly k that 
+# has the highest average value. A subarray is simply a sequence of consecutive elements from the original array. 
+# After identifying this subarray, return the average value, rounded to two decimal places.
+
+# Example #1
+# Input: nums = [1, 2, -5, -3, 10, 3], k = 3
+
+# Output: 3.33
+
+# Explanation: The subarray here is [-3, 10, 3] , so the average is 3.33.
+
+# Example #2
+# Input: nums = [9], k = 1
+
+# Output: 9.00
+
+# Explanation: The subarray here is just [9], so its average is exactly 9.00.
+
+from functools import reduce
+
+def max_avg_subarray(nums, k):
+	ls = []
+	for i in range(len(nums)-k+1):
+		ls.append(nums[i:i+k])
+	dict1 = {}
+	for i in ls:
+		dict1[reduce(lambda x,y:x+y, i)] = i
+	print(dict1)
+	ct = -100000
+	for i in dict1.keys():
+		if i > ct:
+			ct = i
+	print(dict1[ct])	
+
+max_avg_subarray([1, 2, -5, -3, 10, 3], 3)
