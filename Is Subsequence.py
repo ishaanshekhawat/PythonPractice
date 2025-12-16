b17c342648fae0b0eea241eb90fdc3a448827292
Ishaan Singh Shekhawat <ishaan.singh.shekhawat@gmail.com>
2025-12-16 11:29:05 +0530

    Create  Is Subsequence.py

---
+++ Is Subsequence.py
@@ -0,0 +1,32 @@
+# Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+# A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters 
+# without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+# Example 1:
+# Input: s = "abc", t = "ahbgdc"
+# Output: true
+
+# Example 2:
+# Input: s = "axc", t = "ahbgdc"
+# Output: false
+
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        k = 0
+        i = 0
+        if s == "":
+            return True
+        
+        if len(s) > len(t):
+            return False
+
+        while k < len(s):
+            if s[k] == t[i]:
+                k+=1
+            i+=1
+            if i == len(t):
+                break
+        
+        if k == len(s):
+            return True
+        return False
