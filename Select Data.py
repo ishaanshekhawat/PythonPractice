5fc3942d8c3333b9fe2e4f67a8c9d052b8b4c42b
Ishaan Singh Shekhawat <ishaan.singh.shekhawat@gmail.com>
2026-02-03 12:41:58 +0530

    Create Select Data.py

---
+++ Select Data.py
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/select-data/description/
+
+import pandas as pd
+
+def selectData(students: pd.DataFrame) -> pd.DataFrame:
+    res = students[students['student_id'] == 101]
+    return res[['name','age']]
