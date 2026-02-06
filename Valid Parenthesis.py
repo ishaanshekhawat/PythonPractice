ea53e173d90941feeba5a3aab2ae4f7e2634e183
Ishaan Singh Shekhawat <ishaan.singh.shekhawat@gmail.com>
2026-02-07 00:49:24 +0530

    Create Valid Parenthesis.py

---
+++ Valid Parenthesis.py
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/valid-parentheses/
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        while '()' in s or '{}' in s or '[]' in s:
+            s = s.replace('()','').replace('{}','').replace('[]','')       
+        return s == ''
