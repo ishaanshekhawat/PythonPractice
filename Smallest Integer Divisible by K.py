05cb3e1c068a5c65c53d6f366a571be43db94f2c
Ishaan Singh Shekhawat <ishaan.singh.shekhawat@gmail.com>
2026-03-01 16:52:59 +0530

    Create Smallest Integer Divisible by K.py

---
+++ Smallest Integer Divisible by K.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/smallest-integer-divisible-by-k/description/
+
+class Solution:
+    def smallestRepunitDivByK(self, k: int) -> int:
+        if k % 2 == 0 or k % 5 == 0:
+            return -1
+        
+        rem = 0
+
+        for i in range(1, k+1):
+            rem = (rem * 10 + 1) % k
+            if rem == 0:
+                return i
+                
+        return -1
