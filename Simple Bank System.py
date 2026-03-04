4391c94e9b02dd10d11cde13786918db73e6a10b
Ishaan Singh Shekhawat <ishaan.singh.shekhawat@gmail.com>
2026-03-04 23:20:16 +0530

    Create Simple Bank System.py

---
+++ Simple Bank System.py
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/simple-bank-system/description/
+
+class Bank:
+    def __init__(self, balance: List[int]):
+        self.balance = balance
+        self.n = len(balance)
+
+    def transfer(self, account1: int, account2: int, money: int) -> bool:
+        if 1 <= account1 <= self.n and 1 <= account2 <= self.n:
+            if self.balance[account1-1] >= money:
+                self.balance[account1-1] -= money
+                self.balance[account2-1] += money
+                return True
+        return False
+
+    def deposit(self, account: int, money: int) -> bool:
+        if 1 <= account <= self.n:
+            self.balance[account-1] += money
+            return True
+        return False
+
+    def withdraw(self, account: int, money: int) -> bool:
+        if 1 <= account <= self.n and self.balance[account-1] >= money:
+            self.balance[account-1] -= money
+            return True
+        return False
